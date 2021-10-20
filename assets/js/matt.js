057aba30d55c026320724f494e6e5191d10d0739
Matthew Cross <matthewcross1017@gmail.com>
2021-10-20 18:40:16 -0400

    Final Functionality

--- assets/js/matt.js
+++ assets/js/matt.js
@@ -43,32 +43,52 @@ var data = '{"date":"20211020","quantity":"45"}';
 // Turn string into current date
 // Add moment.date() to test function
 
-var currentDate = moment().format('YYYYMMDD');
+var currentDate = moment().date(-27).format('YYYYMMDD');
 var newData = data.replace('20211020',currentDate);
 console.log(newData);
 $('#createtable').click( xhr.send(newData));
 
 // Change Quantity
-//  var PT1 = JSON.parse(localStorage.getItem("PT1"));
-//   variable.dataset.value = PT1[0].criteria1;
-//     PT1[0].criteria2;
-//     var PT2 = JSON.parse(localStorage.getItem("PT2"));
-//     PT2[0].criteria1;
+var profileLocal = JSON.parse(localStorage.getItem("PT1"));
+var criteriaValue1 = profileLocal[0].criteria1;
+// var criteriaValue2 =  profileLocal[0].criteria2;
+console.log(criteriaValue1)
 //     PT2[0].criteria2;
-//     var PT3 = JSON.parse(localStorage.getItem("PT3"));
-//     PT3[0].criteria1;
+var profileLocal2 = JSON.parse(localStorage.getItem("PT2"));
+var criteriaValue2 = profileLocal2[0].criteria1;
+
 //     PT3[0].criteria2;
-//     var PT4 = JSON.parse(localStorage.getItem("PT4"));
-//     PT4[0].criteria1;
+var profileLocal3 = JSON.parse(localStorage.getItem("PT3"));
+var criteriaValue3 = profileLocal3[0].criteria1;
+
 //     PT4[0].criteria2;
+var profileLocal4 = JSON.parse(localStorage.getItem("PT4"));
+var criteriaValue4 = profileLocal4[0].criteria1;
+
 
 // add values together for total tasks completed
-
+var newValue = criteriaValue1 + criteriaValue2 + criteriaValue3 + criteriaValue4
+console.log(newValue);
 // to string() method to turn value into string
-
+newValueString = newValue.toString();
+console.log(newValueString);
 // replace quantity with submitted tasks 
-// var newQuantity = data.replace('quantity', )
+var newQuantity = data.replace('quantity', newValueString )
 
-$('#submitTable').click( xhr.send(newQuantity));
+var url = "https://pixe.la/v1/users/matt-crpss/graphs/project";
 
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.setRequestHeader( "X-USER-TOKEN", "yaaaaaaa");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+var data = '{"date":"20211020","quantity":"45"}';
+ $('#submitTable').click( xhr.send(newQuantity));
+console.log(newQuantity);
 console.log(data);
